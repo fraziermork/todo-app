701a88adb0fc996897976b97be0691cb3a3eafc9
Frazier Mork <fraziermork@gmail.com>
2016-07-04 00:10:57 -0700

    built out webpack config -- loads bootstrap from sass, populates API url from env variables, auto requires all sass files in app/components, uses autoprefixer and postcss, auto provides name-view.html files as template to name-directive.js files, implements html loading, copies over index.html for you to eliminate redundancy with gulp, implements basics needed to differentiate between production and development webpack config

---
+++ app/entry.js
@@ -0,0 +1,5 @@
+require('!!file?name=[name].[ext]!./main/index.html');
+require('./style/base.scss');
+
+
+require('./main/todo-app');
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const gulp    = require('gulp');
+const eslint  = require('gulp-eslint');
+// const del     = require('del');
+// const webpack = require('webpack-stream');
+// const config  = require(`${__dirname}/webpack.config.js`);
+
+const PATHS   = {
+  js:     [`${__dirname}/app/**/*.js`, `${__dirname}/test/**/*.js`, './server.js'],
+  index:  `${__dirname}/app/main/index.html`, 
+  entry:  `${__dirname}/app/entry.js`,
+  build:  `${__dirname}/build`
+};
+
+gulp.task('eslint', () => {
+  return gulp.src(PATHS.js)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+// gulp.task('app:clear', () => {
+//   return del(PATHS.build + '/*');
+// });
+// 
+// gulp.task('app:html', () => {
+//   return gulp.src(PATHS.index)
+//   .pipe(gulp.dest(PATHS.build));
+// });
+// 
+// gulp.task('app:js', () => {
+//   // return gulp.src(PATHS.entry)
+//   //   .pipe(webpack(config))
+//   //   .pipe(gulp.dest(PATHS.build));
+// });
+
+// gulp.task('app:build', ['app:clear', 'app:html', 'app:js'], () => {
+//   
+// });
