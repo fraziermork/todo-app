ffd4050384594419eea45ffd698c402da57e2fc1
Frazier Mork <fraziermork@gmail.com>
2016-08-02 19:00:42 -0700

    mindor changes to server

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app     = express();
+const port    = process.env.API_PORT || 8080;
+
+app.use(express.static(`${__dirname}/build`));
+
+app.listen(port, () => {
+  console.log('App open on port ', port); // eslint-ignore-line
+});
