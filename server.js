84dabe49534493a6c1cab7fd85bcc0df0cb85316
Frazier Mork <fraziermork@gmail.com>
2016-08-03 17:59:36 -0700

    switched to PORT as server variable name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app     = express();
-const port    = process.env.API_PORT || 8080;
+const port    = process.env.PORT || 8080;
 
 app.use(express.static(`${__dirname}/build`));
 
